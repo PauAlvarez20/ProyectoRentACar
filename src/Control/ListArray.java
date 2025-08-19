63a5e6e0d845ccf806145c09ee0574e22e342c64
Ricardo Gallardo <gallardoespinozar@gmail.com>
2025-08-19 13:59:46 -0600

    se crea la interface para el arrayList
    se crea la interface para el ArrayList

---
+++ src/Control/ListArray.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Control;
+
+/**
+ *
+ * @author galla
+ */
+public interface ListArray <T>{
+    public void add(T t);
+    
+    public void remove(T t);
+    
+    public T find(String cedula);
+    
+}
