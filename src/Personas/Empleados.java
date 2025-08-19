a42c004fa51462583d7c1219c6c0afef4eabdaca
Ricardo Gallardo <gallardoespinozar@gmail.com>
2025-08-19 13:40:00 -0600

    se crea la clase empleado
    se crea la clase empleado heredado de persona  con sus metodos de acceso y su constructor

---
+++ src/Personas/Empleados.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Personas;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author galla
+ */
+public class Empleados extends Personas {
+    private String puesto;
+    private double salario;
+
+    public String getPuesto() {
+        return puesto;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setPuesto(String puesto) {
+        this.puesto = puesto;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public Empleados(String puesto, double salario, String cedula, String nombre, LocalDate fechaNacimiento, String telefono, String correo) {
+        super(cedula, nombre, fechaNacimiento, telefono, correo);
+        this.puesto = puesto;
+        this.salario = salario;
+    }
+    
+}
