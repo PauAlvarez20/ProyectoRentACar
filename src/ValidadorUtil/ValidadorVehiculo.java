d6fceaa142cf6fdef344917e1ace1f81f46135f9
CamilaCC <cacoreaco@est.utn.ac.cr>
2025-08-27 22:04:04 -0600

    Clase ValidadorVehiculo
    Se hacen modificaciones de mejora en esta clase

--- src/ValidadorUtil/ValidadorVehiculo.java
+++ src/ValidadorUtil/ValidadorVehiculo.java
@@ -7,6 +7,7 @@ package ValidadorUtil;
 import Vehiculo.EstadoVehiculo;
 import Vehiculo.TipoVehiculo;
 import java.time.Year;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -14,25 +15,34 @@ import java.time.Year;
  */
 public class ValidadorVehiculo {
     
-    public static void validarYear(int year) throws Exception{
-        int yearActual = Year.now().getValue();
-        if(year > yearActual || (yearActual - year) > 20){
-            throw new Exception("El año del vehiculo es invalido");
+    public static void validarYear(int year) {
+      int yearActual = Year.now().getValue();
+        if (year > yearActual || (yearActual - year) > 20) {
+            JOptionPane.showMessageDialog (null, "El año del vehículo es inválido", "Error de Validación",
+                    JOptionPane.ERROR_MESSAGE
+            );
         }  
+        
     }
     
-    public static void validarTipo(TipoVehiculo tipo) throws Exception {
-       if (tipo == null) {
-           throw new Exception("El tipo de vehiculo es invalido");
-           
-       }
+    public static boolean validarTipo(TipoVehiculo tipo) {
+        if (tipo == null) {
+            JOptionPane.showMessageDialog(null, "El tipo de vehículo es inválido", "Error de Validación",
+                    JOptionPane.ERROR_MESSAGE
+            );
+            return false;
+        }
+        return true;
     }
     
-    public static void validarEstado(EstadoVehiculo estado) throws Exception {
-     if (estado == null){
-        throw new Exception("El estado del vehiculo es invalido");
+    public static boolean validarEstado(EstadoVehiculo estado) {
+        if (estado == null) {
+            JOptionPane.showMessageDialog(null, "El estado del vehículo es inválido", "Error de Validación",
+                    JOptionPane.ERROR_MESSAGE
+            );
+            return false;
         }   
-    } 
-    
+        return true;
+    }
  }
 
